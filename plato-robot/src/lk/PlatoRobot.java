249714bdb5c2b1c5b5de66c0354d89357959a19d
LK <lenny.khazan@gmail.com>
2016-03-23 19:16:44 -0400

    Game recording/uploading completed

--- plato-robot/src/lk/PlatoRobot.java
+++ plato-robot/src/lk/PlatoRobot.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,8 +20,6 @@ import robocode.WinEvent;
 public class PlatoRobot extends AdvancedRobot {
 
 	final boolean LEARN = true;
-	final double EPISILON = LEARN ? 0.3 : 0.0;
-	final String UPLOAD_URL = "http://localhost:8080/upload";
 	
 	private BufferedWriter output;
 	
@@ -44,56 +40,14 @@ public class PlatoRobot extends AdvancedRobot {
 	}
 	
 	// When we scan a robot, record a new state.
-	// It may be a problem that the timing between states isn't consistent, but doing
-	// that would mean that we're learning on old data, which is probably even worse.
 	public void onScannedRobot(ScannedRobotEvent event) {
 		this.write(this.getHeading() + " " + this.getEnergy() + " " + event.getBearing() + " " + event.getEnergy());
-	}
-	
-	public void upload() throws IOException {
-		if (!LEARN) return;
-		
-		String boundary = Long.toHexString(System.currentTimeMillis());
-		System.out.println("Opening connection");
-		URLConnection connection = new URL(UPLOAD_URL).openConnection();
-		System.out.println("Done");
-		connection.setDoOutput(true);
-		connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-		System.out.println("Set request stuff");
-		// http://stackoverflow.com/questions/2469451/upload-files-from-java-client-to-a-http-server
-		OutputStream netOutput = connection.getOutputStream();
-	    PrintWriter netWriter = new PrintWriter(new OutputStreamWriter(netOutput, "UTF-8"), true);
-	    for (File file : this.getDataDirectory().listFiles()) {
-	    	System.out.println(file);
-	    	netWriter.append("--" + boundary).append("\r\n");
-	    	netWriter.append("Content-Disposition: form-data; name=\"textFile\"; filename=\"" + file.getName() + "\"").append("\r\n");
-	    	netWriter.append("Content-Type: text/plain; charset=" + "UTF-8").append("\r\n");
-	    	netWriter.append("\r\n").flush();
-	    	System.out.println(file);
-	        Files.copy(file.toPath(), netOutput);
-	        System.out.println(file);
-	        netOutput.flush();
-	        netWriter.append("\r\n").flush();
-	        System.out.println(file);
-	    }
-	    netWriter.append("--" + boundary + "--").append("\r\n").flush();
-	    System.out.println("Sent files");
-	    int responseCode = ((HttpURLConnection)connection).getResponseCode();
-	    System.out.println(responseCode);
-	    if (responseCode != 200) {
-	    	System.out.println("RECEIVED " + responseCode + " WHEN UPLOADING FILES");
-	    } else {
-	    	for (File file : this.getDataDirectory().listFiles()) {
-	    		file.delete();
-	    	}
-	    }
 	}
 	
 	public void onWin(WinEvent event) {
 		this.write("W");
 		try {
 			this.output.close();
-			this.upload();
 		} catch (IOException e) {
 			System.out.println("COULDN'T WRITE/UPLOAD FILE");
 		}
--- plato-server/server.py
+++ plato-server/server.py
@@ -1,4 +1,9 @@
 import tornado.httpserver, tornado.ioloop, tornado.web
+import os
+from pymongo import MongoClient
+
+games = None
+games_history = None
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -9,12 +14,55 @@ class Application(tornado.web.Application):
 
 class UploadHandler(tornado.web.RequestHandler):
     def post(self):
-        print self.request.files
+        if games == None or games_history == None:
+            print('[!] Ignoring upload because database has not been initalized')
+            return
+
+        for filename, array in self.request.files.items():
+            f = array[0]
+
+            if len(f.body) < 5:
+                continue
+
+            if games.find_one({'name': os.path.splitext(filename)[0]}) != None:
+                print('[!] Skipping %s; already uploaded' % filename)
+                continue
+
+            history = []
+            lines = f.body.decode('utf-8').splitlines()
+            if lines[-1] != 'W' and lines[-1] != 'L':
+                lines.append('L')
+
+            result = lines.pop()
+
+            states = [state.split(' ') for state in lines]
+
+            bad_data = False
+            for state in states:
+                if len(state) != 4:
+                    bad_data = True
+                    break
+
+            if bad_data:
+                continue
+
+            for state in lines[:-1]:
+                nums = state.split(' ')
+
+                history.append({'heading': float(nums[0]), 'energy': float(nums[1]), 'oppBearing': float(nums[2]), 'oppEnergy': float(nums[3])})
+
+            doc = {'name': os.path.splitext(filename)[0], 'history': history, 'res': lines[-1]}
+            games.insert_one(doc)
+            games_history.insert_one(doc)
 
 def main():
+    global games, games_history
+    games = MongoClient('localhost', 27017).plato.games
+    games_history = MongoClient('localhost', 27017).plato.games_history
+
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(8080)
-    print 'Starting HTTP server on port 8080'
+    print('Starting HTTP server on port 8080')
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
